c1eb6813289f81b8f58b2e0c8f31e33195a2d6db
ijansa <jaan.edgar@gmail.com>
2013-10-31 13:10:46 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
 print "Tere"
+Eesnimi = raw_input('Mis su eesnimi on?:')
+Perekonnanimi = raw_input('Mis su perekonnanimi on?:')
+
+Eesnimi = Eesnimi.upper()
+Perekonnanimi = Perekonnanimi.upper()
+
+print Eesnimi.upper();
+print Perekonnanimi.lower();
+print Eesnimi.capitalize();
+
+
+if Eesnimi == "Jaan-Edgar":
+	print "Sinu nimi on " + Eesnimi + " " + Perekonnanimi
+else:
+
+	print "ei tunne sind"
